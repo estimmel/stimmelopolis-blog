3c6f21323ce5a0aec4d67d207fdc0614c6a6e9a8
Eric Stimmel <eric.stimmel@som.com>
2020-04-15 01:36:02 -0700

    text replacement of header to format as yaml

--- migration.ps1
+++ migration.ps1
@@ -9,20 +9,38 @@ $destination = $(".\_posts\")
 # Get-ChildItem $source -Filter *.txt -Recurse | Rename-Item -NewName { $_.Directory.Name + ".md" }
 # Get-ChildItem $source -Filter *.md -Recurse | Rename-Item -NewName { "article.text.txt" }
 Get-ChildItem -Recurse $source -Filter *.txt | ForEach-Object {
-    #Select-String
     $newfilename = $_.Directory.Name + ".md" -replace '([0-9]{4})([0-9]{2})([0-9]{2})', '$1-$2-$3'
-    #$filecontents = Get-Content $_.FullName.ToString() -replace '[\-]{4}', ''
     Write-Debug ("Old Filename:    " + $_.FullName)
     Write-Debug ("Post Title:      " + $_.Directory.Name)
     Write-Debug ("New Filename:    " + $destination + $newfilename)
     Write-Debug ("--- ---")
-    $filecontents_orig = Get-Content -Raw $_.FullName
-    $filecontents_orig | Select-String '\n[\-]{4}' -AllMatches | Foreach { # regex replacement in file contents not working yet
-        $_.Matches
-    }
+    $filecontents_orig = (Get-Content $_.FullName -Raw) | Foreach-Object {
+        $_ -replace "^", "---`r`nlayout: post`r`n" `
+           -replace "(?ms)`r`n[\-]{4}", "" `
+           -replace "(?ms)^Author:", "author:" `
+           -replace "(?ms)^Date:", "date:" `
+           -replace "(?ms)^Title:", "title:" `
+           -replace "(?ms)^Description:", "description:" `
+           -replace "(?ms)^Tags:", "tags:" `
+           -replace "(?ms)^Text:", "---" `
+           -replace "`nSlug:.*", ""
+        }
+    Write-Debug ("Modified File Contents: " + $filecontents_orig)
     Write-Debug ("--- ---")
     # Copy-Item -Path $_.FullName -Destination $($destination + $_.Directory.Name + ".md")
 }
+
+<# $original_file = 'path\filename.abc'
+$destination_file =  'path\filename.abc.new'
+(Get-Content $original_file) | Foreach-Object {
+    $_ -replace 'something1', 'something1aa' `
+       -replace 'something2', 'something2bb' `
+       -replace 'something3', 'something3cc' `
+       -replace 'something4', 'something4dd' `
+       -replace 'something5', 'something5dsf' `
+       -replace 'something6', 'something6dfsfds'
+    } | Set-Content $destination_file #>
+
 
 <# $masterfile = $("master.txt")
 Set-Content -Path $masterfile "master list of files"
